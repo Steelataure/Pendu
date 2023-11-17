dcb3f15676f971f42ee49f3abde9d2d3d9166fdd
Steel <alexandre0312@orange.fr>
2023-11-17 11:02:12 +0100

    random word

--- main.c
+++ main.c
@@ -5,14 +5,10 @@
 #define NOMBRES_ESSAIS = 6;
 
 int main(void) {
-<<<<<<< HEAD
     printf(">>> JEU DU PENDU <<<\n");
     char* motChoisi = random_word();
     secret_word(motChoisi);    
-=======
 
-    random_word();
->>>>>>> f8ce7213b924684b2525e3224befee8af4265d23
     return 0;
 }
 
