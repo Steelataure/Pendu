77cdf73bee6ba96507133a629541bb62e389c1e8
Steel <59340020+Steelataure@users.noreply.github.com>
2023-12-16 18:54:54 +0100

    raylib

--- src/Graphique/cmake-build-debug/_deps/raylib-build/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/thomas/CLionProjects/Pendu/cmake-build-debug/_deps/raylib-src/src
-# Build directory: /Users/thomas/CLionProjects/Pendu/cmake-build-debug/_deps/raylib-build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
