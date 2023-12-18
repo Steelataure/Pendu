8290d090eaf295a886a9beb3226bf22fef1de962
TireuDelite <anthony.buffet80@gmail.com>
2023-12-18 22:29:42 +0100

    Debug Themes

--- main.c
+++ main.c
@@ -1,13 +1,7 @@
 #include "raylib.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-<<<<<<< HEAD
 typedef enum { MAIN_MENU, NEW_GAME, CREDITS, RULES, THEMES } GameState;
-=======
-typedef enum { MAIN_MENU, LEVELS, GAME, CREDITS, RULES } GameState;
->>>>>>> d4683dda3cf9cf95bdf833dd68639d41421cc0e0
 
 Rectangle newGameButtonBounds;
 Rectangle rulesButtonBounds;
@@ -30,29 +24,16 @@ Texture2D themesBackgroundTexture;
 GameState gameState = MAIN_MENU;
 bool rulesWindow = false;
 
-const char* animaux[] = {"lion", "elephant", "girafe", "tigre", "singe", "chien"};
-const char* fruits[] = {"pomme", "banane", "fraise", "kiwi", "orange"};
-const char* pays[] = {"France", "Etats-Unis", "Japon", "Bresil", "Australie"};
-const char* metiers[] = {"medecin", "infirmier", "professeur", "architecte", "informaticien"};
-const char* sports[] = {"football", "basketball", "tennis", "natation", "golf"};
-const char* couleurs[] = {"rouge", "vert", "bleu", "jaune", "noir", "rose", "marron"};
-
-
-const char* TheWord(const char* theme);
 void DrawMainMenu(void);
 void DrawNewGame(void);
 void DrawCredits(void);
 void DrawRules(void);
-<<<<<<< HEAD
-void DrawThemes(void);
-=======
-void DrawJeu(void);
-
->>>>>>> d4683dda3cf9cf95bdf833dd68639d41421cc0e0
+const char* DrawThemes();
 
 int main(void) {
     // Initialisation de la fenêtre
     InitWindow(800, 600, "Pendu");
+
     // Chargement de l'image de fond de la page MENU
     Image background = LoadImage("assets/pendu.png");
     backgroundTexture = LoadTextureFromImage(background);
@@ -67,7 +48,6 @@ int main(void) {
     Image rulesBackground = LoadImage("assets/regles.png");
     rulesBackgroundTexture = LoadTextureFromImage(rulesBackground);
     UnloadImage(rulesBackground);
-
 
     // Chargement de l'image de fond pour le MENU de DIFFICULTES
     Image difficultyBackground = LoadImage("assets/niveaux.png");
@@ -109,11 +89,11 @@ int main(void) {
 // Centre les boutons de la page "Themes"
     int totalbisButtonsWidth = 1 * buttonWidthbis + 1 * buttonSpacingbis; // Largeur totale des six boutons et deux espaces entre eux
     animalsButtonBounds = (Rectangle){(screenWidth - totalbisButtonsWidth) / 2, screenHeight / 2 - 30, buttonWidthbis, 50};
-    // fruitsButtonBounds = (Rectangle){animalsButtonBounds.x + buttonWidthbis + buttonSpacingbis, screenHeight / 2 - 30, buttonWidthbis, 50};
-    // countryButtonBounds = (Rectangle){fruitsButtonBounds.x + buttonWidthbis + buttonSpacingbis, screenHeight / 2 - 30, buttonWidthbis, 50};
-    // workButtonBounds = (Rectangle){countryButtonBounds.x + buttonWidthbis + buttonSpacingbis, screenHeight / 2 - 30, buttonWidthbis, 50};
-    // sportsButtonBounds = (Rectangle){workButtonBounds.x + buttonWidthbis + buttonSpacingbis, screenHeight / 2 - 30, buttonWidthbis, 50};
-    // colorsButtonsBounds = (Rectangle){sportsButtonBounds.x + buttonWidthbis + buttonSpacingbis, screenHeight / 2 - 30, buttonWidthbis, 50}; 
+    fruitsButtonBounds = (Rectangle){animalsButtonBounds.x + buttonWidthbis + buttonSpacingbis, screenHeight / 2 - 30, buttonWidthbis, 50};
+    countryButtonBounds = (Rectangle){fruitsButtonBounds.x + buttonWidthbis + buttonSpacingbis, screenHeight / 2 - 30, buttonWidthbis, 50};
+    workButtonBounds = (Rectangle){countryButtonBounds.x + buttonWidthbis + buttonSpacingbis, screenHeight / 2 - 30, buttonWidthbis, 50};
+    sportsButtonBounds = (Rectangle){workButtonBounds.x + buttonWidthbis + buttonSpacingbis, screenHeight / 2 - 30, buttonWidthbis, 50};
+    colorsButtonsBounds = (Rectangle){sportsButtonBounds.x + buttonWidthbis + buttonSpacingbis, screenHeight / 2 - 30, buttonWidthbis, 50}; 
 
     while (!WindowShouldClose()) {
         switch (gameState) {
@@ -122,16 +102,15 @@ int main(void) {
                 PlayMusicStream(musique);
                 UpdateMusicStream(musique);
                 break;
-            case LEVELS:
+            case NEW_GAME:
                 DrawNewGame();
                 PlayMusicStream(musique);
                 UpdateMusicStream(musique);
-                // if ( gameState == THEMES ) { // Si l'utilisateur choisis un niveau de difficulté
-                //     DrawThemes();
-                // }
                 break;
-            case GAME:
-                DrawJeu();
+            case THEMES:
+                DrawThemes();
+                PlayMusicStream(musique);
+                UpdateMusicStream(musique);
                 break;
             case CREDITS:
                 DrawCredits();
@@ -154,117 +133,17 @@ int main(void) {
     UnloadTexture(rulesBackgroundTexture);
     UnloadTexture(difficultyBackgroundTexture);
     UnloadMusicStream(musique);
+
     CloseAudioDevice();
     CloseWindow();
 
     return 0;
 }
 
-static bool motSecretChoisi = false;
-const char* motSecret = NULL;
-
-
-void DrawJeu(void) {
-    BeginDrawing();
-    Color customColor = (Color){253, 231, 190, 255};
-    ClearBackground(customColor);
-
-    static bool motSecretChoisi = false;
-    static const char* motSecret = NULL;
-
-    if (!motSecretChoisi) {
-        // Choisissez le mot secret
-        motSecret = TheWord("animaux");
-
-        // Indiquez que le mot secret a été choisi
-        motSecretChoisi = true;
-    }
-
-    // Utilisez la variable motSecret ici
-    if (motSecret != NULL) {
-        // Dessinez le mot caché
-        DrawText("Mot caché:", 100, 50, 20, BLACK);
-        for (int i = 0; i < strlen(motSecret); i++) {
-            if (motSecret[i] == ' ') {
-                DrawText(" ", 100 + i * 30, 80, 20, BLACK);
-            } else {
-                DrawText("_", 100 + i * 30, 80, 20, BLACK);
-            }
-        }
-        // Dessinez les lettres déjà devinées
-        DrawText("Lettres devinées:", 100, 150, 20, BLACK);
-        //DrawText(motSecret, 100, 100, 30, BLACK);
-
-    }
-
-    EndDrawing();
-}
-
-// Déclarations de variables globales
-char playerName[64] = "";
-bool inputName = false;
-
-// Fonction pour gérer la saisie de texte
-void HandleTextInput(void) {
-    DrawText("Entrez votre nom:", 250, 250, 20, BLACK);
-    DrawRectangleLines(250, 280, 300, 40, BLACK);
-    DrawText(playerName, 260, 290, 20, BLACK);
-
-    // Obtenez la saisie du clavier
-    int key = GetKeyPressed();
-    if (key != 0) {
-        int length = strlen(playerName);
-        if (key == KEY_BACKSPACE && length > 0) {
-            playerName[length - 1] = '\0'; // Supprimez le dernier caractère
-        } else if (length < sizeof(playerName) - 1) {
-            playerName[length] = (char)key; // Ajoutez le caractère
-            playerName[length + 1] = '\0';   // Assurez-vous de terminer la chaîne
-        }
-    }
-}
-
-
-const char* TheWord(const char* theme) {
-    const char** word_array = NULL;
-    int array_size = 0;
-
-    if (strcmp(theme, "animaux") == 0) {
-        word_array = animaux;
-        array_size = sizeof(animaux) / sizeof(animaux[0]);
-    } else if (strcmp(theme, "fruits") == 0) {
-        word_array = fruits;
-        array_size = sizeof(fruits) / sizeof(fruits[0]);
-    } else if (strcmp(theme, "pays") == 0) {
-        word_array = pays;
-        array_size = sizeof(pays) / sizeof(pays[0]);
-    } else if (strcmp(theme, "metiers") == 0) {
-        word_array = metiers;
-        array_size = sizeof(metiers) / sizeof(metiers[0]);
-    } else if (strcmp(theme, "sports") == 0) {
-        word_array = sports;
-        array_size = sizeof(sports) / sizeof(sports[0]);
-    } else if (strcmp(theme, "couleurs") == 0) {
-        word_array = couleurs;
-        array_size = sizeof(couleurs) / sizeof(couleurs[0]);
-    } else {
-        fprintf(stderr, "Theme non trouve\n");
-        exit(EXIT_FAILURE);
-    }
-
-    int random_index = rand() % array_size;
-
-    // Ajoutez un message pour déboguer
-    printf("Theme : %s, Mot secret : %s\n", theme, word_array[random_index]);
-
-    return word_array[random_index];
-}
-
-
 
 // Fonction de la page du MENU
 void DrawMainMenu(void) {
     BeginDrawing();
-
 
     // Affichage de l'image de fond
     DrawTexture(backgroundTexture, 0, 0, RAYWHITE);
@@ -280,7 +159,7 @@ void DrawMainMenu(void) {
     if (CheckCollisionPointRec(GetMousePosition(), newGameButtonBounds)) {
         DrawRectangleLinesEx(newGameButtonBounds, 2, WHITE);
         if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
-            gameState = LEVELS;
+            gameState = NEW_GAME;
         }
     }
 
@@ -313,10 +192,12 @@ void DrawMainMenu(void) {
 }
 
 
+
+
+
 // Fonction de la page "Nouvelle Partie"
 void DrawNewGame(void) {
     BeginDrawing();
-
 
     ClearBackground(RAYWHITE);
 
@@ -331,11 +212,7 @@ void DrawNewGame(void) {
     if (CheckCollisionPointRec(GetMousePosition(), easyButtonBounds)) {
         DrawRectangleLinesEx(easyButtonBounds, 2, WHITE);
         if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
-<<<<<<< HEAD
-            // gameState = THEMES;
-=======
-            gameState = GAME;
->>>>>>> d4683dda3cf9cf95bdf833dd68639d41421cc0e0
+            gameState = THEMES;
         }
     }
 
@@ -347,7 +224,7 @@ void DrawNewGame(void) {
     if (CheckCollisionPointRec(GetMousePosition(), intermediateButtonBounds)) {
         DrawRectangleLinesEx(intermediateButtonBounds, 2, WHITE);
         if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
-            // gameState = THEMES;
+            gameState = THEMES;
         }
     }
 
@@ -361,10 +238,6 @@ void DrawNewGame(void) {
         if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
             gameState = THEMES;
         }
-    }    
-    
-    if (inputName) {
-        HandleTextInput();
     }
 
     // Affichage du bouton "Retour"
@@ -381,6 +254,10 @@ void DrawNewGame(void) {
 
     EndDrawing();
 }
+
+
+
+
 
 
 // Fonction de la page "Crédits"
@@ -405,14 +282,11 @@ void DrawCredits(void) {
     EndDrawing();
 }
 
-<<<<<<< HEAD
 
 
 
 
 
-=======
->>>>>>> d4683dda3cf9cf95bdf833dd68639d41421cc0e0
 // Fonction de la page "Règles du jeu"
 void DrawRules(void) {
     if (rulesWindow) {
@@ -437,10 +311,9 @@ void DrawRules(void) {
         EndDrawing();
     }
 }
-<<<<<<< HEAD
 
 //Fonction de la page Themes
-void DrawThemes(void) {
+const char* DrawThemes() {
     BeginDrawing();
 
     ClearBackground(RAYWHITE);
@@ -461,87 +334,85 @@ void DrawThemes(void) {
         }
     }
 
-    // // Affichage du bouton fruits
-    // DrawRectangleRec(fruitsButtonBounds, BROWN);
-    // DrawText("Fruits", (int)(fruitsButtonBounds.x + fruitsButtonBounds.width / 2 - MeasureText("Fruits", 20) / 2), (int)(fruitsButtonBounds.y + 15), 20, BLACK);
+    // Affichage du bouton fruits
+    DrawRectangleRec(fruitsButtonBounds, BROWN);
+    DrawText("Fruits", (int)(fruitsButtonBounds.x + fruitsButtonBounds.width / 2 - MeasureText("Fruits", 20) / 2), (int)(fruitsButtonBounds.y + 15), 20, BLACK);
 
-    // //Pointeur du bouton "Fruits"
-    // if (CheckCollisionPointRec(GetMousePosition(), fruitsButtonBounds)) {
-    //     DrawRectangleLinesEx(fruitsButtonBounds, 2, WHITE);
-    //     if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
-    //         return "fruits";
-    //         // Mettre ici l'affichage du pendu selon Fruits
-    //     }
-    // }
+    //Pointeur du bouton "Fruits"
+    if (CheckCollisionPointRec(GetMousePosition(), fruitsButtonBounds)) {
+        DrawRectangleLinesEx(fruitsButtonBounds, 2, WHITE);
+        if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
+            return "fruits";
+            // Mettre ici l'affichage du pendu selon Fruits
+        }
+    }
 
-    // // Affichage du bouton pays
-    // DrawRectangleRec(countryButtonBounds, BROWN);
-    // DrawText("Pays", (int)(countryButtonBounds.x + countryButtonBounds.width / 2 - MeasureText("Pays", 20) / 2), (int)(countryButtonBounds.y + 15), 20, BLACK);
+    // Affichage du bouton pays
+    DrawRectangleRec(countryButtonBounds, BROWN);
+    DrawText("Pays", (int)(countryButtonBounds.x + countryButtonBounds.width / 2 - MeasureText("Pays", 20) / 2), (int)(countryButtonBounds.y + 15), 20, BLACK);
 
-    // //Pointeur du bouton "Pays"
-    // if (CheckCollisionPointRec(GetMousePosition(), countryButtonBounds)) {
-    //     DrawRectangleLinesEx(countryButtonBounds, 2, WHITE);
-    //     if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
-    //         return "pays";
+    //Pointeur du bouton "Pays"
+    if (CheckCollisionPointRec(GetMousePosition(), countryButtonBounds)) {
+        DrawRectangleLinesEx(countryButtonBounds, 2, WHITE);
+        if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
+            return "pays";
 
-    //         // Mettre ici l'affichage du pendu selon Pays
-    //     }
-    // }
+            // Mettre ici l'affichage du pendu selon Pays
+        }
+    }
 
-    // // Affichage du bouton métiers
-    // DrawRectangleRec(workButtonBounds, BROWN);
-    // DrawText("Métiers", (int)(workButtonBounds.x + workButtonBounds.width / 2 - MeasureText("Métiers", 20) / 2), (int)(workButtonBounds.y + 15), 20, BLACK);
+    // Affichage du bouton métiers
+    DrawRectangleRec(workButtonBounds, BROWN);
+    DrawText("Métiers", (int)(workButtonBounds.x + workButtonBounds.width / 2 - MeasureText("Métiers", 20) / 2), (int)(workButtonBounds.y + 15), 20, BLACK);
 
-    // //Pointeur du bouton "métiers"
-    // if (CheckCollisionPointRec(GetMousePosition(), workButtonBounds)) {
-    //     DrawRectangleLinesEx(workButtonBounds, 2, WHITE);
-    //     if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
-    //         return "metiers";
-    //         // Mettre ici l'affichage du pendu selon métiers
-    //     }
-    // }
+    //Pointeur du bouton "métiers"
+    if (CheckCollisionPointRec(GetMousePosition(), workButtonBounds)) {
+        DrawRectangleLinesEx(workButtonBounds, 2, WHITE);
+        if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
+            return "metiers";
+            // Mettre ici l'affichage du pendu selon métiers
+        }
+    }
 
-    // // Affichage du bouton Sports
-    // DrawRectangleRec(sportsButtonBounds, BROWN);
-    // DrawText("Sports", (int)(sportsButtonBounds.x + sportsButtonBounds.width / 2 - MeasureText("Sports", 20) / 2), (int)(sportsButtonBounds.y + 15), 20, BLACK);
+    // Affichage du bouton Sports
+    DrawRectangleRec(sportsButtonBounds, BROWN);
+    DrawText("Sports", (int)(sportsButtonBounds.x + sportsButtonBounds.width / 2 - MeasureText("Sports", 20) / 2), (int)(sportsButtonBounds.y + 15), 20, BLACK);
 
-    // //Pointeur du bouton "sports"
-    // if (CheckCollisionPointRec(GetMousePosition(), sportsButtonBounds)) {
-    //     DrawRectangleLinesEx(sportsButtonBounds, 2, WHITE);
-    //     if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
-    //         return "sports";
-    //         // Mettre ici l'affichage du pendu selon sports
-    //     }
-    // }
+    //Pointeur du bouton "sports"
+    if (CheckCollisionPointRec(GetMousePosition(), sportsButtonBounds)) {
+        DrawRectangleLinesEx(sportsButtonBounds, 2, WHITE);
+        if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
+            return "sports";
+            // Mettre ici l'affichage du pendu selon sports
+        }
+    }
 
-    // // Affichage du bouton couleurs
-    // DrawRectangleRec(colorsButtonsBounds, BROWN);
-    // DrawText("Couleurs", (int)(colorsButtonsBounds.x + colorsButtonsBounds.width / 2 - MeasureText("Couleurs", 20) / 2), (int)(colorsButtonsBounds.y + 15), 20, BLACK);
+    // Affichage du bouton couleurs
+    DrawRectangleRec(colorsButtonsBounds, BROWN);
+    DrawText("Couleurs", (int)(colorsButtonsBounds.x + colorsButtonsBounds.width / 2 - MeasureText("Couleurs", 20) / 2), (int)(colorsButtonsBounds.y + 15), 20, BLACK);
 
-    // //Pointeur du bouton "Couleurs"
-    // if (CheckCollisionPointRec(GetMousePosition(), colorsButtonsBounds)) {
-    //     DrawRectangleLinesEx(colorsButtonsBounds, 2, WHITE);
-    //     if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
-    //         return "couleur";
-    //         // Mettre ici l'affichage du pendu selon Couleurs
-    //     }
-    // }
+    //Pointeur du bouton "Couleurs"
+    if (CheckCollisionPointRec(GetMousePosition(), colorsButtonsBounds)) {
+        DrawRectangleLinesEx(colorsButtonsBounds, 2, WHITE);
+        if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
+            return "couleur";
+            // Mettre ici l'affichage du pendu selon Couleurs
+        }
+    }
 
-    // // Affichage du bouton "Retour"
-    // DrawRectangleRec(backButtonBounds, BROWN);
-    // DrawText("Retour", (int)(backButtonBounds.x + backButtonBounds.width / 2 - MeasureText("Retour", 16) / 2), (int)(backButtonBounds.y + 5), 16, BLACK);
+    // Affichage du bouton "Retour"
+    DrawRectangleRec(backButtonBounds, BROWN);
+    DrawText("Retour", (int)(backButtonBounds.x + backButtonBounds.width / 2 - MeasureText("Retour", 16) / 2), (int)(backButtonBounds.y + 5), 16, BLACK);
 
-    // // Pointeur du bouton "Retour"
-    // if (CheckCollisionPointRec(GetMousePosition(), backButtonBounds)) {
-    //     DrawRectangleLinesEx(backButtonBounds, 2, WHITE);
-    //     if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
-    //         gameState = NEW_GAME;
-    //     }
-    // }
+    // Pointeur du bouton "Retour"
+    if (CheckCollisionPointRec(GetMousePosition(), backButtonBounds)) {
+        DrawRectangleLinesEx(backButtonBounds, 2, WHITE);
+        if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
+            gameState = NEW_GAME;
+        }
+    }
 
     EndDrawing();
 }
 
 
-=======
->>>>>>> d4683dda3cf9cf95bdf833dd68639d41421cc0e0
